8db5f942ad4626730a06cef5cd5c8aeace63c871
unknown <Jon@MircaleMachine.(none)>
2012-04-29 01:09:44 -0400

    dded config file with a working jambase api keydded config file with a working jambase api key

---
+++ config.inc.php
@@ -0,0 +1,18 @@
+<!-- TEAM 7 (we pwns noobs) --> 
+
+<?php 
+	/** 
+	 ** MySQL Database Configuration File 
+	 **/ 
+	 
+	//$configure[$sqlType = "mysql"; 
+	$config['host'] = "localhost"; 
+	$config['user'] = "root"; 
+	$config['pass'] = ""; 
+	$config['dbname'] = "finalProject"; 
+	
+	//Jambase API key
+	$config['jamkey'] = "t9hmvhux6mt4a2bt5s962m94";
+	
+	
+?>
