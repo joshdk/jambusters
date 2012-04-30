c8c05e51f6d0943bf553ed570956d6eadee9a3e7
Josh Komoroske <jdkomo.dev@gmail.com>
2012-04-30 14:56:02 -0400

    Reorganized bootstrap files.

--- src/index.php
+++ src/index.php
@@ -12,14 +12,14 @@
     <![endif]-->
 
     <!-- Le styles -->
-    <link href="../bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 60px;
         padding-bottom: 40px;
       }
     </style>
-    <link href="../bootstrap/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
 
     <!-- Le fav and touch icons -->
     <link rel="shortcut icon" href="images/favicon.ico">
@@ -89,19 +89,19 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="../bootstrap/docs/assets/js/jquery.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-transition.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-alert.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-modal.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-dropdown.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-scrollspy.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-tab.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-tooltip.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-popover.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-button.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-collapse.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-carousel.js"></script>
-    <script src="../bootstrap/docs/assets/js/bootstrap-typeahead.js"></script>
+    <script src="/assets/js/jquery.js"></script>
+    <script src="/assets/js/bootstrap-transition.js"></script>
+    <script src="/assets/js/bootstrap-alert.js"></script>
+    <script src="/assets/js/bootstrap-modal.js"></script>
+    <script src="/assets/js/bootstrap-dropdown.js"></script>
+    <script src="/assets/js/bootstrap-scrollspy.js"></script>
+    <script src="/assets/js/bootstrap-tab.js"></script>
+    <script src="/assets/js/bootstrap-tooltip.js"></script>
+    <script src="/assets/js/bootstrap-popover.js"></script>
+    <script src="/assets/js/bootstrap-button.js"></script>
+    <script src="/assets/js/bootstrap-collapse.js"></script>
+    <script src="/assets/js/bootstrap-carousel.js"></script>
+    <script src="/assets/js/bootstrap-typeahead.js"></script>
 
   </body>
 </html>
