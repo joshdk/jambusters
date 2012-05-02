cb334e8a185b27ae8c055bf6832c6560d29b9fd5
Jonathan Kriss <kriss.jonathan@gmail.com>
2012-05-02 03:38:28 -0400

    getEventsByArtist accets a jquery selector for the html item to put API return into

--- src/api/core/lastfmfunctions.js
+++ src/api/core/lastfmfunctions.js
@@ -1,16 +1,29 @@
-function getEventsByArtist(artistName)
+
+/*
+artistName = name of the artist.
+selector = 	html object to put html into. ex. $('#test')
+			appends html to #test
+			
+//TODO Need to implement error checking
+//for bad band name, and maybe return something if no events fond
+*/
+function getEventsByArtist(artistName,selector)
 {
-	url = "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist=" + artistName + "&api_key=b25b959554ed76058ac220b7b2e0a026&format=json";
+	var url = "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist=" + artistName + "&api_key=b25b959554ed76058ac220b7b2e0a026&format=json&callback=?";
+	retVal = '';
    $.getJSON(url, function(data) {
-        var html = '';
 		console.log(data);
-			if(data.events.total!=0)
-			{
+		var html = '';
+		if(data.events.total!=0)
+		{
 			$.each(data.events.event, function(i, item) {
-            html += "<li> Title:" + item.title + "<br> Description:" + item.description + "</li>";
+			html += "<li> Title:" + item.title + "<br> Description:" + item.description + "</li>";
 			});
-			}
-        $('#test').append(html);
-          topArt = data.topartists;
+		}
+		else 
+		{
+			html += "<p> No Events Found </p>";
+		}
+		selector.append(html);
     });
 }
