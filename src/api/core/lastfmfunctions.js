c7697e1ded5cd35ba30cabad1b86af20e8160256
Jonathan Kriss <kriss.jonathan@gmail.com>
2012-05-02 13:34:49 -0400

    added getArtistByPartialArtist function. when given a partial artist will display all the matches

--- src/api/core/lastfmfunctions.js
+++ src/api/core/lastfmfunctions.js
@@ -27,13 +27,15 @@ function getEventsByArtist(artistName,selector)
     });
 }
 
+//Artist will be optionally included
+
 function getEventsByLatLong(Lat, Long,selector,artistName)
 {
 	var url = "http://ws.audioscrobbler.com/2.0/?method=geo.getevents&lat=" + Lat + "&long=" + Long + "&api_key=b25b959554ed76058ac220b7b2e0a026&format=json&callback=?";
    $.getJSON(url, function(data) {
 		console.log(data);
 		var html = '';
-		if(data.events.total!=0)
+		if(data.events.total!=0) //no events found
 		{
 			$.each(data.events.event, function(i, item) {
 			html += "<li> Title:" + item.title + "<br> Description:" + item.description + "</li>";
@@ -46,3 +48,27 @@ function getEventsByLatLong(Lat, Long,selector,artistName)
 		selector.append(html);
     });
 }
+
+function getArtistsByPartialArtist(artist,selector)
+{
+	if(artist == "")
+		return;
+	selector.empty();
+	var url = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + artist + "&api_key=b25b959554ed76058ac220b7b2e0a026&format=json&callback=?";
+	$.getJSON(url, function(data) {
+		console.log(data);
+		var html = '';
+		if(data.results.totalResults!=0) //no artists found
+		{
+			$.each(data.results.artistmatches.artist, function(i, item) {
+			html += "<li>" + item.name + "</li>";
+			});
+		}
+		else 
+		{
+			html += "<p> No Events Found </p>";
+		}
+		selector.append(html);
+    });
+}
+
