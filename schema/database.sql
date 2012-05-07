058b99fcea96b2a0fdb10015464a047a8bf62b7b
Josh Komoroske <jdkomo.dev@gmail.com>
2012-05-07 16:08:54 -0400

    Updated Scheme.
    - Now contains an 'admin' field.

--- schema/database.sql
+++ schema/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 03, 2012 at 03:40 AM
+-- Generation Time: May 07, 2012 at 03:59 PM
 -- Server version: 5.5.21
 -- PHP Version: 5.3.10
 
@@ -31,9 +31,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(32) NOT NULL,
   `hash` varchar(128) NOT NULL,
   `salt` varchar(32) NOT NULL,
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `hash`, `salt`, `admin`) VALUES
+(12, 'admin', '8d6f98290a0408c2d95e419ca70cc054365c1c57fdfc1198359ec9996a9b2dfc145a4fdb5c904c384a0f82948b7ab20f1744d036b8e7824376ee3e91e477f38f', 'J3bPHqJltbofagfRfIYIKjiiRTYrtJNf', 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
